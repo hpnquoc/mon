592613b273ca0749e3ccadfdb18056a1e01a7da2
longpham <longpham3105@gmail.com>
2025-03-28 18:32:26 +0900

    Refactor

---
+++ src/mon/nn/modules/attention/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Implements attention layers."""
+
+from __future__ import annotations
+
+import mon.nn.modules.attention.channel
+import mon.nn.modules.attention.galerkin
+import mon.nn.modules.attention.pixel
+import mon.nn.modules.attention.simam
+import mon.nn.modules.attention.squeeze_excite
+from mon.nn.modules.attention.channel import *
+from mon.nn.modules.attention.galerkin import *
+from mon.nn.modules.attention.pixel import *
+from mon.nn.modules.attention.simam import *
+from mon.nn.modules.attention.squeeze_excite import *
